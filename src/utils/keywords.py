bca3d6ca1f15f287991488568c1f8a1cc208c4ac
Christopher Brückner <christopher.brueckner95@gmail.com>
2020-12-12 18:08:28 +0100

    defaulted the stoplist to NLTK's english stoplist as it leads to more useful results in handpicked samples

--- src/utils/keywords.py
+++ src/utils/keywords.py
@@ -2,19 +2,26 @@ from typing import List
 import RAKE
 import numpy as np
 from tqdm import tqdm
+from nltk.corpus import stopwords
 
 def extract_keyword_str(document: str,
                          maxWords: int = 5,
-                         minScore: float = 0.0) -> List:
+                         minScore: float = 0.0,
+                         useNLTK: bool = True) -> List:
     """Extract the top keywords using RAKE
     Arguments:
     - document: A string containing the text to extract from
     - maxWords: An integer indicating how many keywords to extract
     - minScore: A float that filters out any keywods with lower Score than this
+    - useNLTK:  A boolean to choose between NLTK's stoplist and RAKE's SmartStoplist
 
     Return:
     - keywords: A list of the keywords in the document"""
-    rake = RAKE.Rake(RAKE.SmartStopList())
+    if useNLTK:
+        stoplist = stopwords.words('english')
+    else:
+        stoplist = RAKE.SmartStopList()
+    rake = RAKE.Rake(stoplist)
     keywords = rake.run(document) # dont use maxWords here, in case we filter words with minScore
     keywords = [k for k in keywords if k[1] >= minScore]
     keywords = keywords[:maxWords] #Sorted by Score
