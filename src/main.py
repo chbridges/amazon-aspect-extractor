689e2c3c7b33be472514c4babbb221e9e6831e4a
Armand Rousselot <armand.r@live.de>
2021-01-21 22:12:18 +0100

    added SVM and Random Forest baseline models

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 
 import torch
+import numpy as np
+
 from torch.cuda import is_available
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
@@ -9,6 +11,9 @@ from utils.dataloading import load_semeval2015
 from utils.metrics import accuracy, class_balanced_accuracy, cross_entropy, f1_score
 from utils.preprocessing import PreprocessingPipeline
 from utils.sentiment import SentimentDataset, SentimentModel, evaluate_sentiment_model
+from utils.baselines import top_ngrams, SentimentSVM, SentimentForest
+
+
 
 if __name__ == "__main__":
     # path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
@@ -17,39 +22,79 @@ if __name__ == "__main__":
     # reviewtext = [rev["review_body"] for rev in dataset["train"]]
     # keywords = extract_keyword_list(reviewtext, minScore=2.0)
     # print(keywords[:10])
+
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data/ABSA15")
     dataset = load_semeval2015(path, categories=["restaurants"])
     pipeline = PreprocessingPipeline()
     dataloaders = {}
 
     dataset, dict_for, dict_back = pipeline(dataset)
-    for phase in dataset.keys():
-        revtexts, aspects, sentiment = dataset[phase]
-        sentiments = SentimentDataset(revtexts, aspects, sentiment)
-        dataloaders[phase] = DataLoader(sentiments, batch_size=32, shuffle=True)
+    revtexts, aspects, sentiment = dataset["train"]
+    train_set = SentimentDataset(revtexts, aspects, sentiment)
 
-    device = "cuda" if is_available() else "cpu"
-    model = SentimentModel(
-        len(dict_for) + 1,
-        output_size=3,
-        model_name="LSTM_classifier",
-        device=device,
-        normalize_output=False,
-        n_layers=1,
-        embedding_dim=128,
-        hidden_dim=768,
-        dropout=0.4,
-        bidirectional=False,
-    )
-    optimizer = Adam(model.parameters(), lr=0.004, weight_decay=1e-5)
-    scheduler = ReduceLROnPlateau(optimizer, cooldown=10, factor=0.2, verbose=True)
-    criterion = cross_entropy(dataloaders["train"], device=device)
-    # train_sentiment_model(model, optimizer, dataloaders, criterion=criterion,
-    #                       scheduler=scheduler, n_epochs=150, eval_every=1)
-    model.load_state_dict(
-        torch.load(os.path.join("models", model.name + "_best" + ".pth"))[
-            "model_state_dict"
-        ]
-    )
-    for metric in [accuracy, class_balanced_accuracy, f1_score]:
-        evaluate_sentiment_model(model, dataloaders, criterion=metric)
+    revtexts, aspects, sentiment = dataset["val"]
+    val_set = SentimentDataset(revtexts, aspects, sentiment)
+    #----------------------------------------------------------------------------------------
+    #Baseline Models
+    top_x = top_ngrams(train_set)
+    forest = SentimentForest(top_x)
+    svm = SentimentSVM(top_x)
+
+    x_train = torch.stack([event for event, _, _ in train_set], axis=0).numpy()
+    y_train = torch.Tensor([sentiment for _, _, sentiment in train_set]).numpy()
+    y_train = (2*y_train).astype(np.int)
+
+
+    x_val = torch.stack([event for event, _, _ in val_set], axis=0).numpy()
+    y_val = torch.Tensor([sentiment for _, _, sentiment in val_set]).numpy()
+    y_val = (2*y_val).astype(np.int)
+
+    print("Fitting SVM...")
+    svm.fit(x_train, y_train)
+    print("Fitting Random Forest...")
+    forest.fit(x_train, y_train)
+
+    y_svm_train = svm(x_train)
+    y_svm_val = svm(x_val)
+    y_forest_train = forest(x_train)
+    y_forest_val = forest(x_val)
+
+    print("SVM train accuracy: {}".format(np.mean(np.where((y_train).astype(np.int) == y_svm_train.astype(np.int), 1, 0))))
+    print("SVM val accuracy: {}".format(np.mean(np.where((y_val).astype(np.int) == y_svm_val.astype(np.int), 1, 0))))
+    print("Random Forest train accuracy: {}".format(np.mean(np.where((y_train).astype(np.int) == y_forest_train.astype(np.int), 1, 0))))
+    print("Random Forest val accuracy: {}".format(np.mean(np.where((y_val).astype(np.int) == y_forest_val.astype(np.int), 1, 0))))
+
+
+    #----------------------------------------------------------------------------------------
+    #Neural Network
+
+    # for phase in dataset.keys():
+    #     revtexts, aspects, sentiment = dataset[phase]
+    #     sentiments = SentimentDataset(revtexts, aspects, sentiment)
+    #     dataloaders[phase] = DataLoader(sentiments, batch_size=32, shuffle=True)
+    #
+    # device = "cuda" if is_available() else "cpu"
+    # model = SentimentModel(
+    #     len(dict_for) + 1,
+    #     output_size=3,
+    #     model_name="LSTM_classifier",
+    #     device=device,
+    #     normalize_output=False,
+    #     n_layers=1,
+    #     embedding_dim=128,
+    #     hidden_dim=768,
+    #     dropout=0.4,
+    #     bidirectional=False,
+    # )
+    # optimizer = Adam(model.parameters(), lr=0.004, weight_decay=1e-5)
+    # scheduler = ReduceLROnPlateau(optimizer, cooldown=10, factor=0.2, verbose=True)
+    # criterion = cross_entropy(dataloaders["train"], device=device)
+    # # train_sentiment_model(model, optimizer, dataloaders, criterion=criterion,
+    # #                       scheduler=scheduler, n_epochs=150, eval_every=1)
+    # model.load_state_dict(
+    #     torch.load(os.path.join("models", model.name + "_best" + ".pth"))[
+    #         "model_state_dict"
+    #     ]
+    # )
+    # for metric in [accuracy, class_balanced_accuracy, f1_score]:
+    #     evaluate_sentiment_model(model, dataloaders, criterion=metric)
---
+++ src/utils/baselines.py
@@ -0,0 +1,174 @@
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+import torch
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import numpy as np
+
+
+class SentimentSVM(SVC):
+    def __init__(
+        self, ngrams, kernel="poly", mode="classifier", reg=1.0, balanced=True, degree=3
+    ):
+        self.ngrams = ngrams
+        if mode == "classifier":
+            class_weight = "balanced" if balanced else None
+            super().__init__(
+                kernel=kernel, C=reg, class_weight=class_weight, degree=degree
+            )
+        elif mode == "regression":
+            raise (NotImplementedError())
+        else:
+            raise (ValueError("Unknown SVM mode {}".format(mode)))
+
+    def fit(self, x, y):
+        X = self.extract_features(x)
+        return super().fit(X, y)
+
+    def __call__(self, x):
+        X = self.extract_features(x)
+        return super().predict(X)
+
+    def extract_features(self, x):
+        X = np.zeros((len(x), len(self.ngrams)))
+        revs, asps = x[:, :, 0], x[:, :, 1]
+
+        distance_vector = np.zeros(asps.shape)
+        indx = (
+            np.arange(x.shape[1])
+            .repeat(x.shape[0], 0)
+            .reshape(x.shape[0], x.shape[1], 1)
+        )
+        for i, asp in enumerate(asps):
+            indx_asp = np.where(asp == 1)[0]
+            if len(indx_asp):
+                indx_asp = indx_asp.reshape(1, len(indx_asp))
+                dist = np.reciprocal(np.min(np.abs(indx[i] - indx_asp), axis=-1) + 1)
+                distance_vector[i] = dist
+            else:
+                distance_vector[i] = np.ones(distance_vector.shape[1])
+
+        for j, ngram in enumerate(self.ngrams):
+            ngram_occurence = np.all(rolling_window(revs, len(ngram)) == ngram, axis=-1)
+            ngram_occurence = np.concatenate((ngram_occurence, np.zeros((len(revs), len(ngram)-1))), axis=-1)
+            ngram_score = np.sum(ngram_occurence * distance_vector, axis=-1)
+            X[:, j] = ngram_score
+        return X
+
+
+class SentimentForest(RandomForestClassifier):
+    def __init__(
+        self,
+        ngrams,
+        n_trees=25,
+        max_depth=20,
+        min_samples_split=4,
+        bootstrap=True,
+        balanced=True,
+        mode="classifier",
+        pruning=0.0,
+    ):
+        self.ngrams = ngrams
+        if mode == "classifier":
+            class_weight = "balanced_subsample" if balanced else None
+            super().__init__(
+                n_estimators=n_trees,
+                max_depth=max_depth,
+                min_samples_split=min_samples_split,
+                class_weight=class_weight,
+                bootstrap=bootstrap,
+                ccp_alpha=pruning,
+            )
+        elif mode == "regression":
+            raise (NotImplementedError())
+        else:
+            raise (ValueError("Unknown RF mode {}".format(mode)))
+
+    def fit(self, x, y):
+        X = self.extract_features(x)
+        return super().fit(X, y)
+
+    def __call__(self, x):
+        X = self.extract_features(x)
+        return super().predict(X)
+
+    def extract_features(self, x):
+        X = np.zeros((len(x), len(self.ngrams)))
+        revs, asps = x[:, :, 0], x[:, :, 1]
+
+        distance_vector = np.zeros(asps.shape)
+        indx = (
+            np.arange(x.shape[1])
+            .repeat(x.shape[0], 0)
+            .reshape(x.shape[0], x.shape[1], 1)
+        )
+        for i, asp in enumerate(asps):
+            indx_asp = np.where(asp == 1)[0]
+            if len(indx_asp):
+                indx_asp = indx_asp.reshape(1, len(indx_asp))
+                dist = np.reciprocal(np.min(np.abs(indx[i] - indx_asp), axis=-1) + 1)
+                distance_vector[i] = dist
+            else:
+                distance_vector[i] = np.ones(distance_vector.shape[1])
+
+        for j, ngram in enumerate(self.ngrams):
+            ngram_occurence = np.all(rolling_window(revs, len(ngram)) == ngram, axis=-1)
+            ngram_occurence = np.concatenate((ngram_occurence, np.zeros((len(revs), len(ngram)-1))), axis=-1)
+            ngram_score = np.sum(ngram_occurence * distance_vector, axis=-1)
+            X[:, j] = ngram_score
+        return X
+
+
+def top_ngrams(review_dataset, number=100, n=2):
+    rev_texts = [[], [], []]
+
+    for rev, seq_len, sentiment in review_dataset:
+        rev_texts[(2 * sentiment).type(torch.int)].append(
+            " ".join([str(i) for i in rev[:, 0].tolist()])
+        )
+
+    N_0, N_1, N_2 = len(rev_texts[0]), len(rev_texts[1]), len(rev_texts[2])
+
+    counter = CountVectorizer(stop_words=[0], ngram_range=(1, n), min_df=5)
+
+    S = []
+
+    for polarity in range(len(rev_texts)):
+        n_grams = counter.fit_transform(rev_texts[polarity])
+        transformer = TfidfTransformer().fit(n_grams)
+        df = np.reciprocal(transformer.idf_)
+        vocab = np.array(counter.get_feature_names())
+        S.append((vocab, df * len(rev_texts[polarity])))
+
+    combined_vocab = []
+    [combined_vocab.extend(vocab) for vocab, _ in S]
+    combined_vocab = list(set(combined_vocab))
+
+    count_mat = np.zeros((len(S), len(combined_vocab)))
+
+    for s, (vocab, counts) in enumerate(S):
+        for i, count in enumerate(counts):
+            word = vocab[i]
+            index = combined_vocab.index(word)
+            count_mat[s, index] = count
+
+    S_10, S_11, S_12 = count_mat[0], count_mat[1], count_mat[2]
+
+    frac0 = np.log((S_10 + 1) / (N_0 + 1)) / np.log(N_0 + 1)
+    frac1 = np.log((S_11 + 1) / (N_1 + 1)) / np.log(N_1 + 1)
+    frac2 = np.log((S_12 + 1) / (N_2 + 1)) / np.log(N_2 + 1)
+
+    F_0 = np.abs(frac0 - 1 / 2 * (frac1 + frac2))
+    F_1 = np.abs(frac1 - 1 / 2 * (frac0 + frac2))
+    F_2 = np.abs(frac2 - 1 / 2 * (frac0 + frac1))
+
+    F = F_0 + F_1 + F_2
+    top_x = np.argsort(F)[:number]
+    return [[int(x) for x in combined_vocab[t].split(" ")] for t in top_x]
+
+
+def rolling_window(a, size):
+    #From https://stackoverflow.com/questions/7100242/python-numpy-first-occurrence-of-subarray
+    shape = a.shape[:-1] + (a.shape[-1] - size + 1, size)
+    strides = a.strides + (a. strides[-1],)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
