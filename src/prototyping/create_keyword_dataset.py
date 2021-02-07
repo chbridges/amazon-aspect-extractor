418f72c27495eb6f9c81dab8624fe2748b00978c
Christopher Brückner <christopher.brueckner95@gmail.com>
2021-02-07 23:56:17 +0100

    moved keyword filter prototype into separate folder

--- src/prototyping/create_keyword_dataset.py
+++ src/prototyping/create_keyword_dataset.py
@@ -1,22 +1,31 @@
-from utils.dataloading import load_amazon_multilingual, load_semeval2015
-from utils.keywords import extract_keywords_str, extract_keywords_list, keywords_to_dataframe
-import re
+#############################################################
+# This module will only work if placed into the src folder! #
+#############################################################
 
-#data = load_amazon_multilingual("data/amazon-reviews-ml", ['en'])
+from utils.dataloading import load_amazon_multilingual, load_semeval2015
+from utils.keywords import (
+    extract_keywords_list,
+    extract_keywords_str,
+    keywords_to_dataframe,
+)
+
+# data = load_amazon_multilingual("data/amazon-reviews-ml", ['en'])
 data = load_semeval2015("data/ABSA15")
 
-train = data['train']
-val = data['val']
-test = data['test']
+train = data["train"]
+val = data["val"]
+test = data["test"]
 
 full = train + val + test
 
 sample = [full[i].text for i in range(len(full)) if i % (len(full) // 100) == 0]
 
+
 def get_keywords(doc):
     keywords_nltk = [k for k in extract_keywords_str(doc)]
     keywords_smart = [k for k in extract_keywords_str(doc, useNLTK=False)]
     return list(set(keywords_nltk + keywords_smart))
+
 
 keywords = []
 for s in sample:
