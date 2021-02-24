aeda9b80b2440dc7a6dd51ed0dcb657dc5d22370
Christopher Brückner <christopher.brueckner95@gmail.com>
2021-02-24 06:05:43 +0100

    filter classification eval metrics

--- src/utils/keywordfilter.py
+++ src/utils/keywordfilter.py
@@ -15,10 +15,11 @@ class Filter:
         self.dtrain = None
 
         # Important paths
-        self.current_path = os.path.dirname(os.path.abspath(__file__))
-        self.data_path = os.path.join(self.current_path, "../data/")
+        current_path = os.path.dirname(os.path.abspath(__file__))
+        self.data_path = os.path.join(current_path, "../data/")
+        self.model_path = os.path.join(current_path, "../models/")
 
-        # Encoded POS tags
+        # Encoded PoS tags
         tags = list(spacy.glossary.GLOSSARY.keys())
         self.tags = tags[: tags.index("X") + 1]  # we only want the simple pos_ tags
         self.tagmap = {tags[i]: i for i in range(len(tags))}
@@ -50,12 +51,12 @@ class Filter:
                     matrix[i, -1] += 1
 
         self.dtrain = xgb.DMatrix(data=matrix, label=labels, feature_names=self.tags)
-        self.dtrain.save_binary(os.path.join(self.current_path, "keywordfilter.buffer"))
+        self.dtrain.save_binary(os.path.join(self.model_path, "keywordfilter.buffer"))
 
         # Training
         print("Training the XGBoost model...")
         self.bst = xgb.train(params=self.params, dtrain=self.dtrain)
-        self.bst.save_model(os.path.join(self.current_path, "keywordfilter.model"))
+        self.bst.save_model(os.path.join(self.model_path, "keywordfilter.model"))
 
         return self.bst
 
@@ -63,9 +64,9 @@ class Filter:
         print("Loading XGBoost model...")
         try:
             self.bst = xgb.Booster()
-            self.bst.load_model(os.path.join(self.current_path, "keywordfilter.model"))
+            self.bst.load_model(os.path.join(self.model_path, "keywordfilter.model"))
             self.dtrain = xgb.DMatrix(
-                os.path.join(self.current_path, "keywordfilter.buffer")
+                os.path.join(self.model_path, "keywordfilter.buffer")
             )
         except xgb.core.XGBoostError:
             print("ERROR: There is no pretrained model, call Filter.train() instead.")
@@ -78,7 +79,7 @@ class Filter:
             return None
 
         if type(keywords) == str:
-            keywords = [keywords]
+            keywords = [keywords]  # Allows the prediction of single terms
 
         matrix = np.zeros((0, len(self.tags)))
 
@@ -95,6 +96,7 @@ class Filter:
 
         return self.bst.predict(data)
 
+    # Cross-Validation
     def cv(self):
         if self.dtrain is None:
             print("ERROR: Model is not trained")
@@ -107,6 +109,67 @@ class Filter:
         print("test-rmse-mean: ", np.mean(results["test-rmse-mean"]))
         print("test-rmse-std:  ", np.mean(results["test-rmse-std"]), "\n")
 
+    # Evaluation of classification on the training set for different thresholds
+    def classification_eval(self, threshold=0.3):
+        if self.dtrain is None:
+            print("ERROR: Model is not trained")
+            return
+
+        # Load data
+        data = []
+
+        with open(os.path.join(self.data_path, "filter_train.csv")) as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
+            for row in reader:
+                data.append(row)
+        data = data[1:]
+
+        labels = [bool(int(data[i][1])) for i in range(len(data))]
+        data = [data[i][0] for i in range(len(data))]
+
+        predictions_regression = self.predict(data)
+        predictions_classification = [(x >= threshold) for x in predictions_regression]
+
+        TP = sum(
+            [
+                1
+                for i in range(len(labels))
+                if labels[i] and predictions_classification[i]
+            ]
+        )
+        FP = sum(
+            [
+                1
+                for i in range(len(labels))
+                if not labels[i] and predictions_classification[i]
+            ]
+        )
+        TN = sum(
+            [
+                1
+                for i in range(len(labels))
+                if not labels[i] and not predictions_classification[i]
+            ]
+        )
+        FN = sum(
+            [
+                1
+                for i in range(len(labels))
+                if labels[i] and not predictions_classification[i]
+            ]
+        )
+
+        precision = TP / (TP + FP)
+        recall = TP / (TP + FN)
+        f1_score = 2 * precision * recall / (precision + recall)
+        accuracy = (TP + TN) / len(labels)
+
+        print("Evaluation for threshold =", threshold)
+        print("Precision:", precision)
+        print("Recall:   ", recall)
+        print("F1 Score: ", f1_score)
+        print("Accuracy: ", accuracy, "\n")
+
 
 # Sample usage and some insights for the report
 if __name__ == "__main__":
@@ -116,6 +179,8 @@ if __name__ == "__main__":
         bst.train()
 
     bst.cv()
+
+    bst.classification_eval()
 
     print("good battery life:     ", bst.predict("good battery life"))
     print("I am amazed:           ", bst.predict("I am amazed"))
@@ -128,13 +193,7 @@ if __name__ == "__main__":
         bst.predict("this one is impossible"),
     )
 
-    # Requires matplotlib which is not in Pipfile or requirements.txt
+    from matplotlib import pyplot as plt
 
-    try:
-        from matplotlib import pyplot as plt
-
-        xgb.plot_importance(bst.bst)
-        plt.show()
-
-    except ModuleNotFoundError:
-        pass
+    xgb.plot_importance(bst.bst)
+    plt.show()
